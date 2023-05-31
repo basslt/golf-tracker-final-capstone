afe600fcd4735b2607a2692e1c1d57c71f591f98
Nick Grover <nickg96@gmail.com>
2023-05-31 13:59:29 -0400

    starting league vue pages

---
+++ final-capstone/capstone/vue/src/components/LeaguePageNav.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <button>View All Members</button>
+      <button>Invite Members</button>
+      <button>Schedule Tee Time</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ final-capstone/capstone/vue/src/views/LeagueHome.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <league-leaderboard />
+      <league-page-nav />
+  </div>
+</template>
+
+<script>
+import LeagueLeaderboard from "../components/LeagueLeaderboard.vue"
+import LeaguePageNav from "../components/LeaguePageNav.vue"
+
+export default {
+    components: {
+        LeagueLeaderboard,
+        LeaguePageNav
+    }
+
+}
+</script>
+
+<style>
+
+</style>
