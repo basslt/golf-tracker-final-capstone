629284e7a7ceafd68dfced745fbb7347a2fec089
Nick Grover <nickg96@gmail.com>
2023-06-06 17:48:18 -0400

    fixed message and invite board

--- final-capstone/capstone/vue/src/components/MessageList.vue
+++ final-capstone/capstone/vue/src/components/MessageList.vue
@@ -1,38 +1,79 @@
 <template>
     <div class="container">
-        <div class="message-list">
-            <message-card v-for="message in $store.state.messages" :key="message.id" :message="message" v-on:click="showMessage(message)"/>
+        <div class="main">
+            <div>
+                <button type="button" v-on:click="showChange">Inbox</button>
+                <button type="button" v-on:click="showChange">Sent</button>
+            </div>
+            <div class="message-list">
+                <div class="received-messages" v-if="show===true">
+                    <h2>Received Messages</h2>
+                    <inbox-message-card v-for="message in receivedMessages" :key="message.id" :message="message" v-on:click="showMessage(message)"/>
+                </div>
+                <div class="sent-messages" v-if="show===false">
+                    <h2>Sent Messages</h2>
+                    <sent-message-card v-for="message in sentMessages" :key="message.id" :message="message" v-on:click="showMessage(message)"/>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import messageService from '../services/MessageService'
-import MessageCard from '../components/MessageCard.vue'
+import SentMessageCard from './SentMessageCard.vue'
+import InboxMessageCard from '../components/InboxMessageCard.vue'
 
 export default {
     name: "message-list",
     components: {
-        MessageCard,
+        SentMessageCard,
+        InboxMessageCard
     },
     data() {
         return {
+            messages: [],
+            receivedMessages: [],
+            sentMessages: [],
+            show: true,
+            currentUserId: ''
         }
     },
     methods: {
         getMessages() {
             messageService.getAllMessages().then( (response) => {
-                this.$store.commit("SET_MESSAGES", response.data);
+                this.messages = response.data;
             });
         },
-
+        getCurrentUser() {
+            this.currentUser = this.$store.getters.getUser;
+        },
         showMessage(message) {
             this.$emit('message-clicked', message);
         },
-
+        getReceivedMessages() {
+            const user = this.$store.getters.getUser;
+            const userId = user.id;
+            messageService.getReceivedMessagesByUser(userId).then( (response) => {
+                this.receivedMessages = response.data;
+            });
+        },
+        getSentMessages() {
+           const user = this.$store.getters.getUser;
+           const userId = user.id;
+           messageService.getSentMessagesByUser(userId).then( (response) => {
+               this.sentMessages = response.data;
+           });
+        },
+        showChange() {
+            this.show === true ? this.show = false : this.show = true;
+        },
     },
     created() {
         this.getMessages();
+        this.getReceivedMessages();
+        this.getSentMessages();
+        this.getCurrentUser()
     }
 
 }
@@ -44,10 +85,18 @@ export default {
 .container {
     display: flex;
     flex-direction: column;
+    justify-content: flex-start;
 }
 
 .message-list {
-    width: 50%;
+    /* display: flex;
+    flex-direction: column;
+    width: 50%; */
+}
+
+.main {
+    position: absolute;
+   
 }
 
 </style>
