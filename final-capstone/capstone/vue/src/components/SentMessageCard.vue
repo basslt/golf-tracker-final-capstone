6eef0e65433d8dee0734b43d129de978f445bd63
Nick Grover <nickg96@gmail.com>
2023-06-08 03:45:19 -0400

    message card

--- final-capstone/capstone/vue/src/components/SentMessageCard.vue
+++ final-capstone/capstone/vue/src/components/SentMessageCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="main">
-        <div class="message-card" @click="$emit('click')">
+        <div class="message-card" :class="this.$store.state.messageRead ? 'read' : 'unread'" @click="$emit('click')">
             <h2>{{this.senderUser.username}}</h2>
             <p>{{message.type}}</p>
         </div>
@@ -38,46 +38,74 @@ export default {
 
 /* .message-card{
     border: solid;
-    margin: 10px;
+    margin: 3%;
     width: 800%;
     height: 75px;
     border-radius: 12px;
-    background-color: #6AD6B2;
-    border-color: #059262;
+    background-color: rgb(179, 218, 255);
+    border-left-color: rgb(68, 68, 196);
     border-top-width: 1px;
     border-right-width: 1px;
     border-bottom-width: 1px;
     border-left-width: 7px;
-}
-
-h2{
-    color: white;
 } */
+
+
 .message-card {
   border: none;
   margin: 10px;
   width: 800px;
   height: 75px;
   border-radius: 12px;
-  background-color: #6AD6B2;
-  border-color: #059262;
-  border-top-width: 1px;
+  /* background-color:  #6AD6B2; */
+  /* border-color: #059262;  */
+  border-style: solid;
+  /* border-top-width: 1px;
   border-right-width: 1px;
   border-bottom-width: 1px;
-  border-left-width: 7px;
+  border-left-width: 7px; */
   display: flex;
   align-items: center;
   padding: 10px;
   transition: background-color 0.3s;
+}
+
+.unread {
+  background-color:  #6AD6B2;
+  border-color: #059262;
+  border-top-width: 2px;
+  border-right-width: 2px;
+  border-bottom-width: 2px;
+  border-left-width: 7px;
+}
+
+.read > p {
+  color: black;
+  margin: 0;
+  font-size: 14px;
   
 }
 
-.main :hover{
-   background-color: #7ED9C2; 
-  cursor: pointer;
+.read > h2 {
+  color: black;
+  margin: 0;
+  font-size: 25px;
+  font-weight: bold;
 }
 
+.read {
+  background-color:  white;
+  border-color: #059262;
+  border-top-width: 2px;
+  border-right-width: 2px;
+  border-bottom-width: 2px;
+  border-left-width: 7px;
+}
 
+.main :hover{
+  background-color: #7ED9C2; 
+  cursor: pointer;
+}
 h2 {
   color: white;
   margin: 0;
@@ -90,4 +118,5 @@ p {
   margin: 0;
   font-size: 14px;
 }
+
 </style>
